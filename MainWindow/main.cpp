dc1648802c8f1bf20ea27e9b084366a1dee4afcd
SCnudde <125920286+SCnudde@users.noreply.github.com>
2025-11-28 22:32:52 +0400

    MainWIn witch cursor DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,35 +1,42 @@
-#include<Windows.h>
+#include <Windows.h>
+#include "resource.h"
 
-CONST CHAR g_sz_WINDOW_CLASS[] = "My first window";
+CONST CHAR g_sz_WINDOW_CLASS[] = "Mijn eerste venster";
+CONST CHAR mainMessage[] = "Mijn eerste tekst";
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	WNDCLASSEX wClass;
+	WNDCLASSEX wClass;//создаём экземпляр класса окна,для обращения к членам класса WNDCLASSEX
+	
 	ZeroMemory(&wClass, sizeof(wClass));
 	
-	//Инициализируем размеры и стиль:
+	//Инициализируем поля класса окна размерами и стилями:
 	wClass.style = NULL;
-	wClass.cbSize = sizeof(wClass);//cb_ - Count Bytes
-	wClass.cbClsExtra = 0;
-	wClass.cbWndExtra = 0;
+	wClass.cbSize = sizeof(wClass);//cb_ - Count Bytes. Размер структуры в байтах.
+	wClass.cbClsExtra = 0;// число освобождаемых байтов при создании экземпляра приложения
+	wClass.cbWndExtra = 0;// число освобождаемых байтов в конце структуры
 
 	//Инициализируем внешний вид окон
-	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE (IDI_ICON_RUBLE)); // дескриптор значка
+	wClass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE (IDI_ICON_PALM)); // дескриптор цвета фона окошка
+	//wClass.hIcon = (HICON) LoadImage(NULL, "ruble.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wClass.hIconSm = (HICON)LoadImage(NULL, "palm.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	
+	wClass.hCursor = (HICON) LoadImage(hInstance, "starcraft-original\\Busy.ani", IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wClass.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE (IDC_CURSOR1)); // дескриптор курсора мыши
 	wClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 
 	//Инициализация системных переменных:
-	wClass.hInstance = hInstance;
-	wClass.lpszClassName = g_sz_WINDOW_CLASS;
-	wClass.lpszMenuName = NULL;
-	wClass.lpfnWndProc = WndProc;
+	wClass.hInstance = hInstance;//дескриптор экземпляра приложения
+	wClass.lpszClassName = g_sz_WINDOW_CLASS;// указатель на const-строку, содержащюю имя класса
+	wClass.lpszMenuName = NULL; // указатель на const-строку, содержащюю имя меню, применяемого для класса
+	wClass.lpfnWndProc = WndProc; // указатель на имя пользовательской функции
 
-	if (RegisterClassEx(&wClass) == NULL)
+	if (RegisterClassEx(&wClass) == NULL)// проверяtv, зарегистрирован ли класс
 	{
-		MessageBox(NULL, "Fout bij registratie het Class", NULL, MB_OK | MB_ICONERROR);
+		MessageBox(NULL, "Het registratie het Class is mislukt", NULL, MB_OK | MB_ICONERROR);
 		return 0;
 	}	
 	//2) Создание окна:
@@ -39,15 +46,21 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		NULL, //exStyle
 		g_sz_WINDOW_CLASS,//Имя класса окна
 		g_sz_WINDOW_CLASS,//Заголок окна
-		WS_OVERLAPPEDWINDOW,//Стиль окна, Стили всегда зависят от класса окна
+		WS_OVERLAPPEDWINDOW | WS_VSCROLL,//Стиль окна, Стили всегда зависят от класса окна
 		CW_USEDEFAULT,CW_USEDEFAULT, //Позиция
-		CW_USEDEFAULT,CW_USEDEFAULT, //Размер окна
+		640,480, //Размер окна
 		NULL,
 		NULL, //Для главного окна это ResourceID главного меню,
-		//для дочернего окна (Control) -ResourceID дочернего окна (IDC_BUTTON_COPY)
+				//для дочернего окна (Control) -ResourceID дочернего окна (IDC_BUTTON_COPY)
 		hInstance,
 		NULL
 	);
+
+	if (!hwnd) 
+	{	
+		MessageBox(NULL, "Ontwerp van het venster is mislukt!","FOUT",MB_OK);
+		return 0;
+	}
 
 	//3) Запуск цикла сообщений
 
@@ -56,16 +69,36 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		MessageBox(NULL, "Het venster is niet geslagd", NULL, MB_OK | MB_ICONERROR);
 	}
 
-	ShowWindow(hwnd, nCmdShow);
-	UpdateWindow(hwnd);
+	ShowWindow(hwnd, nCmdShow);//вызывает дескриптор окна и CreateWindow(), 2 параметр вызывает стиль отображения
+						//Задаетрежим отображения окна - Развернуто на весь экран
+	UpdateWindow(hwnd);//обновляет рабочую область окна, отправляя сообщение в WM_PAINT,
+						//если клиентская область окна не пустая
+	MSG msg;
+	while (GetMessage(&msg, NULL, NULL, NULL) > NULL) 
+	{
+		TranslateMessage(&msg);//преобразует сообщения виртуальных клавиш в символьные
+		DispatchMessage(&msg);//Отправляет сообщения в процедуру окна
+	}
+	return msg.wParam;	
 
-	return 0;
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	HDC hDC;
+	PAINTSTRUCT ps;
+	RECT rect; // стр-ра, определяющая размер клиентской области
+	COLORREF colorText = 0x00FF0000;
+
 	switch (uMsg)
-	{
+	{		
+	case WM_PAINT: // если нужно нарисовать, то:
+		hDC = BeginPaint(hwnd, &ps); // инициализируем контекст устройства
+		GetClientRect(hwnd, &rect); // получаем ширину и высоту области для рисования
+		SetTextColor(hDC, colorText); // устанавливаем цвет контекстного устройства
+		DrawText(hDC, mainMessage, -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER); // рисуем текст
+		EndPaint(hwnd, &ps); // заканчиваем рисовать
+		break;
 	case WM_CREATE:
 		break;
 	case WM_COMMAND:
