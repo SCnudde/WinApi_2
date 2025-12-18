ecbc36c9b1d924bbe97ef53788ae4bd850e51fad
SCnudde <125920286+SCnudde@users.noreply.github.com>
2025-12-18 17:09:52 +0500

    Resource-only DLL DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -9,6 +9,7 @@
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 VOID SetSkin(HWND hwnd, CONST CHAR skin[]);
+VOID SetSkinFromDLL(HWND hwnd, CONST CHAR skin[]);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -211,7 +212,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			GetModuleHandle(NULL),
 			NULL
 		);
-		SetSkin(hwnd, "square_blue");
+		SetSkinFromDLL(hwnd, "square_blue");
 	}
 	
 	break;
@@ -487,7 +488,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 
 		InvalidateRect(hwnd, 0, TRUE);
-		SetSkin(hwnd, g_sz_SKIN[skinID]);			
+		SetSkinFromDLL(hwnd, g_sz_SKIN[skinID]);			
 		DestroyMenu(cmMain);
 	}
 	break;
@@ -521,4 +522,24 @@ VOID SetSkin(HWND hwnd, CONST CHAR skin[])
 			);
 		SendMessage(hButton, BM_SETIMAGE, 0, (LPARAM)bmpButton);
 	}
+}
+
+VOID SetSkinFromDLL(HWND hwnd, CONST CHAR skin[])
+{
+	HMODULE hSkin = LoadLibrary(skin);
+	for (int i = IDC_BUTTON_0; i <= IDC_BUTTON_EQUAL; i++) 
+	{
+		HBITMAP hBitmap = (HBITMAP)LoadImage
+		(hSkin,
+			MAKEINTRESOURCE(i),
+			IMAGE_BITMAP,
+			i == IDC_BUTTON_0 ? g_i_DOUBLE_BUTTON_SIZE : g_i_BUTTON_SIZE, //X
+			i == IDC_BUTTON_EQUAL ? g_i_DOUBLE_BUTTON_SIZE : g_i_BUTTON_SIZE,  //Y
+			LR_SHARED  //загрузка из разделяемой библиотеке
+		);
+		SendMessage(GetDlgItem(hwnd, i), BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hBitmap);
+	}
+
+
+	FreeLibrary(hSkin);
 }
